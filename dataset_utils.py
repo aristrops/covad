59938b45dc9222683449fcf673834b92f9f9afd3
Arianna Stropeni <ari.stropeni01@gmail.com>
2025-05-23 08:38:25 +0200

    finished dataset creation pipeline

--- dataset_utils.py
+++ dataset_utils.py
@@ -49,3 +49,32 @@ def first_vlm_query(category, model_name, sample, anomalous = True):
         concept_json = []
     
     return concept_json
+
+
+#second VLM query
+def second_vlm_query(category, model_name, sample, concept_list, anomalous = True):
+    image_path = sample["image_path"]
+    label = sample["label"]
+
+    if anomalous:
+        message = f"You are an expert evaluating an industrial image to detect anomalies. I provide an image of a {category}"\
+                f"The image has been classified as anomalous, which implies that it shows a visible defect, alteration or damage. The defect is {label}."\
+                f"Knowing this, choose which concepts you see in the image among the following list of attributes: {concept_list}."\
+                "Output the result as a JSON object of this form: {concept_1: true, concept_2: false, ...}. Output ONLY the JSON object, nothing else."
+    else:
+        message = f"You are an expert evaluating an industrial image to detect anomalies. I provide an image of a {category}"\
+                "The image has been classified as normal, which implies that there is no visible defect, anomaly or issue."\
+                f"Knowing this, choose which concepts you see in the image among the following list of attributes: {concept_list}."\
+                "Output the result as a JSON object of this form: {concept_1: true, concept_2: false, ...}. Output ONLY the JSON object, nothing else."
+    
+    response = client.chat(model=model_name, messages=[{"role": "user", "content": message, "images": [image_path]}])
+
+    try:
+        concept_json = extract_json(response["message"]["content"])
+    except Exception as e:
+            print(f"Error parsing response for {image_path}: {e}")
+            concept_json = {}
+    
+    vector = concepts_to_vectors(concept_json, concept_list)
+
+    return vector
